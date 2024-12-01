e7c7820348634f1a548431c94bf37e8a2772735b
treva556 <marktreva92@gmail.com>
2024-12-01 19:52:16 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className=' text-red-700 font-mono'> Hello People!</h1>
+    </>
+  )
+}
+
+export default App
